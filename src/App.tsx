507cb52a26e2a4a1e15061ad6ad73442af9fa902
Natig <116652802+NatigAgarzayev@users.noreply.github.com>
2023-12-17 20:42:27 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
     setIsLoading(true)
     setChat(curr => [...curr, { text: { role: "user", content: text } }])
     try {
-      await axios.post("hhttps://my-chat-gpt-server.onrender.com/send-message", {
+      await axios.post("https://my-chat-gpt-server.onrender.com/send-message", {
         content: text
       })
         .then(res => res.data)
