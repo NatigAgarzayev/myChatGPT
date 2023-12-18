494a8b581e8296c8da69edfae6113ab283050a79
NatigAgarzayev <7p7ya11@code.edu.az>
2023-12-18 13:35:22 +0400

    fixed Faq.tsx

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom"
 
 
 export default function Faq() {
+
+    const nav = useNavigate()
     return (
         <div>
             It's a FAQ page.
+            <div onClick={() => nav("/")}>Go home</div>
         </div>
     )
 }
